43e2efd7bf4eddbb8264d37e2de40064278808f2
Vadim Ershov <vadim.ershov.01@mail.ru>
2021-12-07 00:07:53 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { UniversityComponent } from './university/university.component';
+import { ButtonModule } from "primeng/button";
+import { RippleModule } from "primeng/ripple";
+import {PrimeNGConfig} from "primeng/api";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UniversityComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule,
+    RouterModule.forRoot([
+      {path: "university", component: UniversityComponent},
+      {path: '', redirectTo: '/university', pathMatch: 'full'},
+    ]), ButtonModule, RippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+}
